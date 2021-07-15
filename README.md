73dc57bff9a83ede242deabcd7a919162962f149
jerish431 <jerishbaskaran@gmail.com>
2021-07-15 23:17:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-hp6ryy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-hp6ryy)
---
+++ src/app/cdk-drag-drop-overview-example.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Basic Drag&Drop
+ */
+@Component({
+  selector: 'cdk-drag-drop-overview-example',
+  templateUrl: 'cdk-drag-drop-overview-example.html',
+  styleUrls: ['cdk-drag-drop-overview-example.css']
+})
+export class CdkDragDropOverviewExample {}
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
